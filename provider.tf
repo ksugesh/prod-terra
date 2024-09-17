8c38a0a70ecff68a68623842b79a9b0cabf9ee11
ksugesh <k.sugesh@gmail.com>
2024-09-17 19:32:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,22 @@
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.113.0"
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
-#Configuration options
-  subscription_id = "38194960-acc8-4938-9af5-385f9d08d4eb"
-  tenant_id = "9ca24c0e-cf46-4afc-831f-ce88cd603b07"
-  client_id = "a643b26e-9bbb-4776-a49e-2e2acbe55df5"
-  client_secret = ""
-  features {
-  }
+  features {}
 }
